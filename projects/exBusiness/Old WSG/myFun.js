73d951b8aa4673d53ccdf5a293d082a9ac14e3b0
shandowpro <shadisayed.6891@gmail.com>
2023-05-29 16:22:06 +0200

    new push

---
+++ projects/exBusiness/Old WSG/myFun.js
@@ -0,0 +1,63 @@
+
+// Slider Slide Bar:
+/* Set the width of the side navigation to 250px */
+// function openNav() {
+//   document.getElementById("mySidenav").style.width = "170px";
+//   document.getElementById("main").style.marginLeft = "150px";
+
+// }
+
+/* Set the width of the side navigation to 0 */
+// function closeNav() {
+//   document.getElementById("mySidenav").style.width = "0";
+//   document.getElementById("main").style.marginLeft = "0";
+
+// }
+
+// ===================================================================
+// Redirecting page in to other page site by switching check box:
+
+// function redirect() {
+//   var checkbox = document.getElementById('click');
+//   if (checkbox.checked==true) {
+//     alert('checked'); 
+//     window.location = 'https://www.facebook.com';
+//   }
+// }​​​​​​
+
+
+// document.getElementById('click').onclick = function() {
+//   if (this.checked==true)
+//       alert('checked'); 
+//       window.location = 'https://www.facebook.com';
+// }​​​​​​
+
+
+// if (document.getElementById('click').checked) {
+//     alert("your Current page is [indexEN]");  
+//     window.location='https://www.google.com';
+//     // return true;
+//     // window.location='https://www.yous  endit.com/dropbox?dropbox=mydomain';
+
+// } else {
+//     alert("your Current page is [indexAR]");
+//     window.location='file:///C:/Users/hp/Desktop/MyDesigns/Choosen%20Design/Wall%20Street/WallStreet%2011-1-2022/indexAR.html';
+//     return true;
+//     // window.location='https://www.facebook.com';
+//     // window.location='https://www.yousendit.com/dropbox?dropbox=mydomain';
+// }
+// ========================================
+// function redirect ()
+// {
+//     if(document.getElementById('click').checked == "checked")
+//     {
+//         alert('You checked the lang Switcher');
+//         // window.location='https://www.yousendit.com/dropbox?dropbox=mydomain';
+//         // return false;
+//     }
+//     return true;
+// }
+
+
+
+
